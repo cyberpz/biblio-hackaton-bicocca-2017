c61f17fe038b79e56d0f8c28ffc59a72d71ca700
Peppuz <peppuzvitale@gmail.com>
2017-05-11 13:50:46 +0200

    update

--- api.php
+++ api.php
@@ -1,21 +1,29 @@
-if(isset($_GET['add'])){
-        if ( isset($_GET['p']))
-                player_start($_GET['p']);
-}
+<?php
 
-else if(isset($_GET['get'])){
-        if(isset($_GET['id'])){
-                if (isset($_GET['g']))
-                        get_game($_GET['id']);
-                else
-                        get_user($_GET['id']);
+header('Content-Type: text/html; charset=utf-8');
+
+
+if(isset($_GET['add'])){
+        if ( isset($_GET['p'])){
+                player_start($_GET['p']);
         }
-        else if(isset($_GET['q']))
-                get_questions();
+}
+else if(isset($_GET['get'])){
+
+        if(isset($_GET['p'])){
+                if (isset($_GET['g']))
+                        get_game($_GET['p']);
+                else
+                        get_user($_GET['p']);
+        }
+
+        else if( isset($_GET['q']) ){
+                get_ques($_GET['q']);
+        }
 }
 
 function player_start($player){
-  # main function: if queue empty => add to queue else => match
+  // main function: if queue empty => add to queue else => match
   $result = mysqli_fetch_assoc(mysqli_query(db(),"SELECT id, player FROM queue;"));
   if($result['player'] == NULL) add_queue($player);
   else{
@@ -27,53 +35,42 @@ function player_start($player){
 }
 
 function get_game($player){
-        $var  = mysqli_fetch_assoc(mysqli_query(db(),"SELECT * FROM games WHERE player1=$playerr
- or player2 = $player "));
-        print($var['id']);
+        $var  = mysqli_fetch_assoc(mysqli_query(db(),"SELECT * FROM games WHERE player1 = '$player' or player2 = '$player' "));
+        get_ques$var['id']
 }
 
 function add_queue($player){
-        mysqli_query(db(),"INSERT INTO queue (`player`) VALUES ($player)");
+        mysqli_query(db(),"INSERT INTO queue (`player`) VALUES ('$player')");
 }
 
 function add_game($player1,$player2){
-        mysqli_query(db(), "INSERT INTO games (`player1`,`player2`) VALUES ($player1, $player2))
-");
+  mysqli_query(db(), "INSERT INTO games (`player1`,`player2`) VALUES ('$player1', '$player2')");
   # send random questions
-  print(mysqli_fetch_assoc(mysqli_query(db(),"SELECT id FROM games WHERE player1 = $player1;")))
-['id']);
 }
 
 function remove_queue($player){
-  mysqli_query(db(), "DELETE FROM `queue` WHERE player = $player");
+  mysqli_query(db(), "DELETE FROM `queue` WHERE player = '$player'");
 }
 
-function get_questions(){
-  # get 5 random questions w/ answ
-  // get min and  max int
-  $min =mysqli_fetch_assoc(mysqli_query(db(),"SELECT MIN(id) FROM questions"))['MIN(id)'];
-  $max =mysqli_fetch_assoc(mysqli_query(db(),"SELECT MAX(id) FROM questions"))['MAX(id)'];
-  $rand = rand($min,$max);
-
-  $question = mysqli_fetch_assoc(mysqli_query(db(),"SELECT * FROM questions WHERE id = $rand")))
-;
-  $ans = explode('|',$question['a']);
-  $question['a'] = $ans;
-  $question['correct'] = $ans[0];
-  print(json_encode($question));
-
+function get_ques($id_game){
+  include_once 'generator.php';
+  $questions = get_questions($id_game);
+  $arrayName = array('id'=>$id_game,'array'=>$questions);
+  print_r(json_encode($arrayName));
 }
 
 function get_user($player){
-  $user =  mysqli_fetch_assoc(mysqli_query(db(), "SELECT * FROM Users WHERE UID = $player"));
-  if( $user != ''){
-    return JSON.encode($user);
-  }else{
-    return "{}";
-  }
+        $user =  mysqli_fetch_assoc(mysqli_query(db(), "SELECT * FROM Users WHERE UID = $player"));
+        if( $user != ''){
+          return JSON.encode($user);
+        }else{
+          return "{}";
+        }
 }
 
 function  db(){
-  return mysqli_connect('localhost','hack','xampp','hack');
+        return mysqli_connect('localhost','hack','xampp','hack');
+        mysql_set_charset('utf8');
+
 }
 ?>
---
+++ generator.php
@@ -0,0 +1,67 @@
+<?php
+
+//header('Content-Type: application/json');
+
+//isset($_GET['token']) or die("Serve un token partita!");
+
+function get_questions($token){
+
+        $mysqli = new mysqli("localhost", "hack", "xampp", "hack");
+        /* check connection */
+        if ($mysqli->connect_errno) {
+            printf("Connect failed: %s\n", $mysqli->connect_error);
+            exit();
+        }
+
+        // prima parte - verifica che la partita esista
+
+        $q = sprintf("SELECT COUNT(*) FROM `games` WHERE `id` = '%s'", $token);
+        if ($result = $mysqli->query($q)) {
+            /* fetch object array */
+            $row = $result->fetch_row();
+            if ($row[0] == 0)
+               die("Game not found");
+        } else
+            printf("Errormessage: %s\n", $mysqli->error);
+
+        // seconda parte - scarica le domande
+        $json = array();
+        $q = 'SELECT * FROM `questions` ORDER BY RAND() LIMIT 5';
+        if ($result = $mysqli->query($q)) {
+            /* fetch object array */
+            $i = 0;
+            while ($row = $result->fetch_assoc()) {
+                //printf ("%s (%s)\n", $row[0], $row[1]);
+
+                $a = explode('|', $row['a']);
+                $acorrect = $a[0];
+                shuffle($a);
+                $correct = array_search($acorrect, $a);
+                $json[$i] = array(
+                           'q' => $row['q'],
+                           'a' => $a,
+                           'correct' => $correct);
+                $i++;
+            }
+            /* free result set */
+            $result->close();
+        }
+        else {
+            printf("Errormessage: %s\n", $mysqli->error);
+        }
+
+        /* close connection */
+        $mysqli->close();
+        return utf8ize($json);
+}
+
+function utf8ize($d) {
+        if (is_array($d)) {
+            foreach ($d as $k => $v) {
+                $d[$k] = utf8ize($v);
+            }
+        } else if (is_string ($d)) {
+            return utf8_encode($d);
+        }
+        return $d;
+}
